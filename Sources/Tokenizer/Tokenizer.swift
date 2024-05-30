2f53b2666303ba0292f086dad8be61a002b65122
AJ Ibraheem <ibraheemaj@outlook.com>
2024-05-30 10:27:46 +0100

    Staging ...

--- Sources/Tokenizer/Tokenizer.swift
+++ Sources/Tokenizer/Tokenizer.swift
@@ -1,1 +1,57 @@
 import Foundation
+
+struct Token: Hashable, CustomStringConvertible {
+    let value: UInt
+    var description: String {
+        return "\(value)"
+    }
+}
+
+struct TokenPair: Hashable, CustomStringConvertible {
+    let first: Token
+    let second: Token
+    var description: String {
+        return "(\(first),\(second))"
+    }
+    
+    init(first: Token, second: Token) {
+        self.first = first
+        self.second = second
+    }
+    
+    init(_ tuple: (Token, Token)) {
+        self.first = tuple.0
+        self.second = tuple.1
+    }
+    
+    func callAsFunction() -> String {
+        if first.value <= 255 && second.value <= 255 {
+            return "\(UnicodeScalar(UInt8(first.value)))\(UnicodeScalar(UInt8(second.value)))"
+        }else{
+            // This is a merged or compressed token pair - print as is
+            return self.description
+        }
+    }
+}
+
+func encode(_ str: String) -> [Token] {
+    return str.utf8.map({ Token(value: UInt($0)) })
+}
+
+func pairCounts(tokens: [Token], sorted: Bool = false) -> [Dictionary<TokenPair, Int>.Element] {
+    var stats: [TokenPair: Int] = [:]
+    
+    for pair in zip(tokens, tokens[1...]) {
+        let key = TokenPair(pair)
+        stats[key, default: 0] += 1
+    }
+    
+    if sorted {
+        let sortedStats = stats.sorted(by: {(p1,p2) -> Bool in
+            return p1.value > p2.value
+        })
+        return sortedStats
+    }else{
+        return stats.map({ p in p })
+    }
+}
--- Tests/TokenizerTests/TokenizerTests.swift
+++ Tests/TokenizerTests/TokenizerTests.swift
@@ -2,11 +2,31 @@ import XCTest
 @testable import Tokenizer
 
 final class TokenizerTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    let text = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
+    
+    func testEncode() throws {
+        let tokens = encode(text)
+        let sep = String(repeating: "---", count: 10)
+        print(sep)
+        print(text)
+        print("Length: \(text.count)") // Interestingly this is less than the count reported in https://colab.research.google.com/drive/1y0KnCFZvGVf_odSfcNAws6kcDD7HsI0L#scrollTo=-wm3T6JYOhvg&line=1&uniqifier=1
+        print(sep)
+        print(tokens)
+        print("Length: \(tokens.count)")
+    }
+    
+    // TODO: Verify sorting
+    // https://dotnetperls.com/convert-int-character-swift
+    func testPairCounting() throws {
+        let tokens = encode(text)
+        let pairStats = pairCounts(tokens: tokens, sorted: true)
+        
+        print(pairStats)
+        
+        let topPair = pairStats[0]
+        print("\(topPair.key): \(topPair.key())")
+        print(topPair.value)
+        
+        print(tokens)
     }
 }
